52429f00a10c4278b08c61ed9ac1bb692ffb9c48
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-07-12 13:26:49 +0200

    added more entries to gitignore, added macros

--- src/VarSizes.h
+++ src/VarSizes.h
@@ -28,4 +28,12 @@ unsigned long long	ft_strlen(const char *s);
 void		ft_putstr_n(const char *s, unsigned long long total_len);
 void		ft_putnbr_n(long long n, long long total_len, int pad_right);
 void		ft_putnbr_nu(unsigned long long n, unsigned long long total_len);
+
+// thanks to https://stackoverflow.com/a/28338942/2999220
+
+# define MAX_OF(type) \
+    (((type)(~0LLU) > (type)((1LLU<<((sizeof(type)<<3)-1))-1LLU)) ? (long long unsigned int)(type)(~0LLU) : (long long unsigned int)(type)((1LLU<<((sizeof(type)<<3)-1))-1LLU))
+# define MIN_OF(type) \
+    (((type)(1LLU<<((sizeof(type)<<3)-1)) < (type)1) ? (long long int)((~0LLU)-((1LLU<<((sizeof(type)<<3)-1))-1LLU)) : 0LL)
+
 #endif
